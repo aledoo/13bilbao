f4a6f57557822711d05c55c295444ce7bc1e1bf1
Alex Dolara <alexander.dolara@gmail.com>
2024-02-23 12:36:02 +0100

    revert changes

--- js/random.js
+++ js/random.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-    var classCycle=['image-1','image-2','image-3','image-4','image-5','image-6','image-7','image-8','image-9','image-10','image-11','image-12','image-13','image-14','image-15','image-16'];
+    var classCycle=['image-1','image-2','image-3','image-4','image-5','image-6'];
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
     $('body').addClass(classToAdd);
